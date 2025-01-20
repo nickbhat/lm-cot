1de018ab8bd5e52208d878763f8a75a3833e826d
Nick B <nick.bhat@gmail.com>
2025-01-19 20:03:33 -0600

    Switch to tracking max iterations only.

--- train.py
+++ train.py
@@ -12,11 +12,10 @@ from network import Decoder
 
 def train(
         batch_size: int,
+        max_iters: int,
         block_size: int,
         emb_size: int,
-        num_epochs: int,
         hidden_dim: int,
-        epoch_size: int,
 ):
     # Handle paths and 
     data_path = Path(".") / Path("nanoGPT") / Path("data") / Path("shakespeare_char")
@@ -27,8 +26,8 @@ def train(
     train = BlockStreamDataset(data_path / Path("train.bin"), block_size)
     valid = BlockStreamDataset(data_path / Path("val.bin"), block_size)
 
-    train_dl = DataLoader(train, batch_size)
-    valid_dl = DataLoader(valid, batch_size)
+    train_dl = iter(DataLoader(train, batch_size))
+    valid_dl = iter(DataLoader(valid, batch_size))
 
     # Initialize Network 
     vocab_size = metadata["vocab_size"]
@@ -39,39 +38,43 @@ def train(
     loss_fn = nn.CrossEntropyLoss()
 
     # Training Loop
-    for epoch in range(num_epochs):
-        epoch_dl = iter(train_dl)
-        for i in range(epoch_size):
-            batch = next(epoch_dl)
-            # Initial solution: Just slice off a token because shuffling.
-            inp = batch[:, :-1]
-            target = batch[:, 1:]
-            logits = decoder(inp)
-            logits = logits.transpose(1, 2)
-            loss = loss_fn(logits, target)
+    print("Training")
+    it = 0
+    while True:
+        batch = next(train_dl)
+        # Initial solution: Just slice off a token because shuffling.
+        inp = batch[:, :-1]
+        target = batch[:, 1:]
+        logits = decoder(inp)
+        logits = logits.transpose(1, 2)
+        loss = loss_fn(logits, target)
 
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        
+        it += 1
 
-            print(loss.item())
+        if it % 50 == 0:
+            print(f"Iter: {it}, Loss: {loss.item()}") 
+
+        if it > max_iters:
+            break
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=3)
+    parser.add_argument("--max_iters", type=int, default=1000)
     parser.add_argument("--block_size", type=int, default=27)
-    parser.add_argument("--epoch_size", type=int, default=17)
     parser.add_argument("--emb_size", type=int, default=11)
     parser.add_argument("--hidden_dim", type=int, default=13)
-    parser.add_argument("--num_epochs", type=int, default=20)
     args = parser.parse_args()
 
     train(
         batch_size = args.batch_size,
+        max_iters = args.max_iters,
         block_size = args.block_size,
-        epoch_size = args.epoch_size,
-        num_epochs = args.num_epochs,
         emb_size = args.emb_size,
         hidden_dim = args.hidden_dim
     )
